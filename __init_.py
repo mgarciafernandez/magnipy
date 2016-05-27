f0011d71a58ed89b9f358e2121e94b799e5fbc69
mgarciafernandez <manugf90@gmail.com>
2016-05-26 17:27:27 -0700

    Added __init__ file

---
+++ __init_.py
@@ -0,0 +1,22 @@
+"""
+Package:
+	magnipy
+
+Modules:
+	catutils:
+		Functions to handle catalogs and masks.
+		Allows to do txt-fits conversion and define masks as well as mask data.
+	magnipy:
+		Statistical functions to compare with theory.
+	jsonw:
+		Json interface to store 2pacf.
+"""
+
+__version__ = "0.1"
+
+def version():
+	return __version__
+
+import catutils
+import magnipy
+import jsonw
