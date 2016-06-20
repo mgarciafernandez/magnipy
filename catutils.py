b22e12a37e8e689860fdd432aa90cd0f4015502e
mgarciafernandez <manugf90@gmail.com>
2016-06-20 15:51:51 -0700

    Removed multiprocessing support at knn

--- catutils.py
+++ catutils.py
@@ -7,7 +7,7 @@ import sklearn.neighbors
 import collections
 
 
-__NCPU__ = multiprocessing.cpu_count()-1
+__NCPU__ = multiprocessing.cpu_count()
 
 def WriteFitsTable(filename=None,colnames=[],types=[],*arg):
 
@@ -295,40 +295,6 @@ def Recenter(filename=None,colname=[]):
 	cols  = fits.ColDefs(columnlist)
 	tbhdu = fits.BinTableHDU.from_columns(cols)
 	tbhdu.writeto(filename+'_centered')
-
-def ReweightKNN(array_to_match,array_to_reweight,keys,nn=100,ncpu=None):
-	"""
-	Computes the weights of an given a table with N objects on an k-dim space with the KNN approach.
-	The weights are computed such that the k-dim space of M objects of another reference table.
-	-Input:
-		array_to_match (structured array): The reference table.
-		array_to_reweight (structured array): The array for which the weights are going to be computed.
-		keys (list of str): the names of the fields of the array that will compose the k-dim space.
-		nn (int): the number of nearest neighbors.
-		ncpu (int): the number of jobs to use. If not specified, it will be asignated #_of_threads_of_computer-1
-	-Output:
-		w_norm (float array): the weights normalized at the range (0,1).
-	"""
-
-	if ncpu is None:
-		ncpu = __NCPU__
-
-	array_to_match    = numpy.vstack([array_to_match[key_] for key_ in keys]).T
-	array_to_reweight = numpy.vstack([array_to_reweight[key_] for key_ in keys]).T
-
-	tree_match = sklearn.neighbors.NearestNeighbors(n_neighbors=nn+1,n_jobs=ncpu).fit(array_to_match)
-	tree_torew = sklearn.neighbors.NearestNeighbors(n_neighbors=nn+1,n_jobs=ncpu).fit(array_to_reweight)
-
-	dist_torew,indx_torew = tree_torew.kneighbors(array_to_reweight,n_neighbors=nn+1)
-
-	which_nn = numpy.array( [nn]*len(dist_torew) )
-	distance = dist_torew[numpy.arange(len(dist_torew)),which_nn]
-	dist_match,indx_match = tree_match.radius_neighbors(array_to_reweight)
-
-	ww = [float(len(d_)) for d_ in dist_match]
-	w_norm = [ w_/sum(ww) for w_ in ww ]
-
-	return w_norm
 
 def RenameColumn(filein,keysin=[],keysout=[]):
 	"""
--- wutils.py
+++ wutils.py
@@ -41,3 +41,34 @@ def DoRandomFlat(N=1,ra=[],dec=[],masks=[],masknames=[]):
 	cols  = fits.ColDefs(columnlist)
 	tbhdu = fits.BinTableHDU.from_columns(cols)
 	return tbhdu
+
+def ReweightKNN(array_to_match,array_to_reweight,keys,nn=100):
+	"""
+	Computes the weights of an given a table with N objects on an k-dim space with the KNN approach.
+	The weights are computed such that the k-dim space of M objects of another reference table.
+	-Input:
+		array_to_match (structured array): The reference table.
+		array_to_reweight (structured array): The array for which the weights are going to be computed.
+		keys (list of str): the names of the fields of the array that will compose the k-dim space.
+		nn (int): the number of nearest neighbors.
+	-Output:
+		w_norm (float array): the weights normalized at the range (0,1).
+	"""
+
+	array_to_match    = numpy.vstack([array_to_match[key_] for key_ in keys]).T
+	array_to_reweight = numpy.vstack([array_to_reweight[key_] for key_ in keys]).T
+
+	tree_match = sklearn.neighbors.NearestNeighbors(n_neighbors=nn+1).fit(array_to_match)
+	tree_torew = sklearn.neighbors.NearestNeighbors(n_neighbors=nn+1).fit(array_to_reweight)
+
+	dist_torew,indx_torew = tree_torew.kneighbors(array_to_reweight,n_neighbors=nn+1)
+
+	which_nn = numpy.array( [nn]*len(dist_torew) )
+	distance = dist_torew[numpy.arange(len(dist_torew)),which_nn]
+	dist_match,indx_match = tree_match.radius_neighbors(array_to_reweight)
+
+	ww = [float(len(d_)) for d_ in dist_match]
+	w_norm = [ w_/sum(ww) for w_ in ww ]
+
+	return w_norm
+
